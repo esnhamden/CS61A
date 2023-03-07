4aa073e5af5c7bf06aa7846f3d1e9e10713790e5
Eason Hamden <esnhamden@gmail.com>
2023-03-06 23:19:58 -0800

    Implement construct_sent function (question 5)

--- lab/lab05/lab05_extra.py
+++ lab/lab05/lab05_extra.py
@@ -39,7 +39,8 @@ def construct_sent(word, table):
     import random
     result = ''
     while word not in ['.', '!', '?']:
-        "*** YOUR CODE HERE ***"
+        result = result + ' ' + word    
+        word = random.choice(table[word])
     return result.strip() + word
 
 def shakespeare_tokens(path='shakespeare.txt', url='http://composingprograms.com/shakespeare.txt'):
